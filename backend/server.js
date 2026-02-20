058e0ab6f0c71b8a612a27611545e1e10d23ac94
Prakashom18 <shahomprakash097@gmail.com>
2026-02-20 20:29:21 +0545

    configuring server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+app.use(express.json());
+
 
 app.listen(3000,(err)=>{
     console.log('running on port 300')
